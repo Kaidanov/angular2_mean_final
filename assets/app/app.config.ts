69131be792d3ac5df1d457a54587eb0006ec23c7
Kaidanov <2bhere4u@gmail.com>
2016-12-05 12:23:01 +0200

    updating the domain prefix for aws

--- assets/app/app.config.ts
+++ assets/app/app.config.ts
@@ -31,10 +31,9 @@ export class AppConfig {
                     endPoint: 'https://kaidanov-angular2-mean.herokuapp.com/'
                 };
                 break;
-
-            case 'LowCost-env.ppiw7txwdp.us-west-2.elasticbeanstalk.com':
+            case 'lowCost-env.ppiw7txwdp.us-west-2.elasticbeanstalk.com':
                 data = {
-                    endPoint: 'https://LowCost-env.ppiw7txwdp.us-west-2.elasticbeanstalk.com/'
+                    endPoint: 'https://lowCost-env.ppiw7txwdp.us-west-2.elasticbeanstalk.com/'
                 };
                 break;
             default:
